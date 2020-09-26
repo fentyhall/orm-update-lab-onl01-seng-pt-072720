22d7d609451ef39da125c0d1249eccbe9e7d4cf7
fentyhall <fentymhall@gmail.com>
2020-09-26 13:20:21 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -15,22 +15,19 @@ class Student
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
-        grade INTEGER
+        grade TEXT
       )
     SQL
-
     DB[:conn].execute(sql)
   end 
 
   def self.drop_table
-    sql = "DROP TABLE students"
-
+    sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
 
   def update 
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end 
 
@@ -49,16 +46,16 @@ class Student
   end 
 
   def self.create(name, grade)
-    new_student = Student.new(name, grade)
-    new_student.save 
-    new_student
+    student = Student.new(name, grade)
+    student.save 
+    student
   end 
 
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
     grade = row[2]
-    student = self.new(name, grade, id)
+    self.new(name, grade, id)
   end 
 
   def self.find_by_name(name)
@@ -66,6 +63,7 @@ class Student
       SELECT * 
       FROM students 
       WHERE name = ?
+      LIMIT 1
     SQL
 
     DB[:conn].execute(sql, name).map do |row|
